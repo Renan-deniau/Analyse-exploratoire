027d60c4155c11bf3f66179e56a609236654f277
Renan-deniau <d.renan@outlook.fr>
2024-11-05 18:20:11 +0100

    "début code"

--- notebooks/test.py
+++ notebooks/test.py
@@ -1,2 +1,12 @@
 # test
-print("Hello World")
+import pandas as pd
+import numpy as np
+
+print("Hello World!")
+
+df = pd.read_csv(
+    r"E:\Programmation\data-portfolio\1 - Analyse exploratoire\creditcard.csv"
+)
+
+print(df)
+print("Max time:", np.max(df["Time"]) / 3600)
