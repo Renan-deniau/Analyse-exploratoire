0704f9dae2d172a0799e107e310ac3b4f0903bda
Renan-deniau <d.renan@outlook.fr>
2024-11-06 18:13:01 +0100

    tkinter tcl and tk resolved

--- notebooks/test.py
+++ notebooks/test.py
@@ -1,4 +1,7 @@
 # test
+# pip install pandas
+# pip install matplotlib
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,6 +15,7 @@ df = pd.read_csv(
 print(df)
 print("Max time:", np.max(df["Time"]) / 3600)
 print("Mean time:", np.mean(df["Time"]) / 3600)
+
 
 plt.hist(df["Time"], bins=50)
 plt.xlabel("Time")
